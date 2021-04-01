21179da943fd539e6af6bcc5d85eeb5ad26cf821
Nguyen Thanh <hoaint1@ghn.vn>
2021-04-01 16:36:16 +0700

    init...

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
