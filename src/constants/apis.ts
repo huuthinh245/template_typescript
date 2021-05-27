910ce78ef59187874912f987f9cfe3109189a20c
Thinh <thinh@Thinhs-MacBook-Pro.local>
2021-05-27 10:24:29 +0700

    fix

--- src/constants/apis.ts
+++ src/constants/apis.ts
@@ -1,8 +1,8 @@
 const GHNAPI = {
-    URL: 'https://driver-stg-api.ghn.dev/oms',
-    AUTHEN_URL: "https://sso-v2.ghn.dev/internal/logout?app_key=6e3132ca-8833-4db0-8a7a-3447d7a09d63",
-    REDIRECT_URL: "&redirect_uri=http://mpds.ghn.vn/loading?authorcode=",
-    PDS_URL: 'https://lastmile-stg-api.ghn.dev'
+    URL: "",
+    AUTHEN_URL: "",
+    REDIRECT_URL: "",
+    PDS_URL: ""
 }
 
 export  {
